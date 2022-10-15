237231d32813762b75cf6b4d9804069a01321a42
CarlosMol <caliche187@hotmail.com>
2022-10-14 20:58:40 -0500

    pruebas2

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,7 +3,7 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero+2 / numero;
+        return numero+3 / numero;
     }
     
     public Double cubo(Double numero) {
